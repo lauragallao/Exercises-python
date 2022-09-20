4e697e41bab7222d7ccbebfa3a8428ebb6e7da40
lauragallao <71766905+lauragallao@users.noreply.github.com>
2022-09-20 08:53:46 -0300

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,19 @@
+
+
+
+# Exercício 01
+"""
+faça um programa que receba dois numeros e diga qual é o maior:
+"""
+num1 = int(input('Digite o primeiro numero: '))
+num2 = int(input('Digite o segundo numero: '))
+
+if num1> num2:
+    print (f' {num1} é maior que {num2}')
+
+elif num1==num2:
+    print ('Os numeros sao iguais')
+
+else:
+    print(f'{num1} é menor que {num2}')
+
